14d51405f3cc9216bd56ac665a6d8763f311cfef
Hongs <ihongs@live.cn>
2019-05-09 18:16:16 +0800

    数据结构定义

---
+++ hongs-serv-mosaic/src/main/java/io/github/ihongs/serv/mosaic/MosaicPageEntity.java
@@ -0,0 +1,17 @@
+package io.github.ihongs.serv.mosaic;
+
+/**
+ *
+ * @author hong
+ */
+public class MosaicPageEntity extends MosaicEntity {
+    
+    public MosaicPageEntity(String conf, String form) {
+        super(conf, form);
+    }
+
+    public MosaicPageEntity() {
+        super("mosaic", "page");
+    }
+
+}
---
+++ hongs-serv-mosaic/src/main/java/io/github/ihongs/serv/mosaic/MosaicPaneEntity.java
@@ -0,0 +1,17 @@
+package io.github.ihongs.serv.mosaic;
+
+/**
+ *
+ * @author hong
+ */
+public class MosaicPaneEntity extends MosaicEntity {
+    
+    public MosaicPaneEntity(String conf, String form) {
+        super(conf, form);
+    }
+
+    public MosaicPaneEntity() {
+        super("mosaic", "pane");
+    }
+
+}
